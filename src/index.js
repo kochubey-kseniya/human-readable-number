34d297e8b5405454a5e37710e9a16565cf4e7f61
kochubey-kseniya <96043439+kochubey-kseniya@users.noreply.github.com>
2022-02-24 11:05:24 +0300

    refactor: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+    let numbers = [' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+  let dozens = [' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+  let result = [];
+  let hundreds = Math.floor(number / 100);
+  let tens = Math.floor(number % 100 / 10);
+  if(number === 0) {
+    return 'zero';
+  }   
+  if(hundreds != 0) {
+    result.push(numbers[hundreds-1] + ' hundred');
+  } 
+  if(tens >= 2) {
+    result.push(dozens[tens - 2]);
+    result.push(numbers[number%10 - 1]);
+  } else {
+    result.push(numbers[number%100 - 1]);
+  }
+  return result.join('').trim();
 }
